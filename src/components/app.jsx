9511479e86a9aa60afc2638b955cb220cf1f2a28
pureux <pureux@gmail.com>
2016-06-09 23:16:53 -0500

    Add Material UI and use dark theme

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,20 +1,27 @@
 'use strict';
 
 import React from 'react';
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 import Header from './header.jsx';
 import Footer from './footer.jsx';
 
+const darkMuiTheme = getMuiTheme(darkBaseTheme);
+
 export default class App extends React.Component {
   render() {
     return (
-      <div id="app">
-        <Header key="header" />
-        <main>
-          <p>Lorem ipsum dolor sit amet.</p>
-        </main>
-        <Footer key="footer" />
-      </div>
+      <MuiThemeProvider muiTheme={darkMuiTheme}>
+        <div id="app">
+          <Header key="header" />
+          <main>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </main>
+          <Footer key="footer" />
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,13 +1,14 @@
 'use strict';
 
 import React from 'react';
+import RaisedButton from 'material-ui/RaisedButton';
 
 export default class Footer extends React.Component {
   render() {
     return (
       <footer>
-        <button>&lt;</button>
         <small>Wutodo</small>
+        <RaisedButton label="Home" primary={true} />
       </footer>
     );
   }
